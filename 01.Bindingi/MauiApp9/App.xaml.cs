ec948277614b33180f6750ef17374c28e271f8a4
uczeń <x@x>
2026-09-16 14:48:50 +0200

    Bindingi

---
+++ 01.Bindingi/MauiApp9/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiApp9
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new AppShell());
+
+            window.Width = 300;
+            window.Height = 300;
+            window.Title = "Kacper World";
+
+            return window;
+        }
+    }
+}
---
+++ 01.Bindingi/MauiApp9/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace MauiApp9
+{
+    public class Calculator : BindableObject
+    {
+        public float ValueA
+        {
+            get => field;
+            set
+            {
+                field = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public float ValueB
+        {
+            get => field;
+            set
+            {
+                field = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public float Result
+        {
+            get
+            {
+                return ValueA + ValueB;
+            }
+        }
+
+    }
+}
